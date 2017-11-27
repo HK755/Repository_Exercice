2cb2e96b5dba80406feee5a02c0174b1308f75a6
HK755 <34021549+HK755@users.noreply.github.com>
2017-11-27 09:55:13 +0100

    Vérification de l'id et du mot de passe.

--- Les_conditions.py
+++ Les_conditions.py
@@ -13,3 +13,20 @@ Conditions multiples	 : and (ET)
 						   or (OU)
 						   in / not in (DANS / PAS DANS)
 """
+
+identifiant = "Jason"
+mot_de_passe = "123"
+
+print("Interface Utilisateur")
+
+user_id = input("Entrer votre identifiant : ")
+user_password = input("Entrer votre mot de passe : ")
+
+if user_id == identifiant and user_password == mot_de_passe:
+	print("Vous êtes connectés. Bienvenue", user_id)
+elif user_id != identifiant and user_password == mot_de_passe:
+	print("L'identifiant est incorrecte.")
+elif user_password != mot_de_passe and user_id == identifiant:
+	print("Le mot de passe est incorrecte.")
+else:
+	print("Le mot de passe et l'identifiant sont incorrectes.")
