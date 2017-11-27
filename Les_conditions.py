edd259eb738bdebf1b30f58a16cb0c6f4de1137b
unknown <karasu.h@protonmail.com>
2017-11-27 09:51:32 +0100

    Ajout d'un commentaire expliquant le fonctionnement des conditions.

--- Les_conditions.py
+++ Les_conditions.py
@@ -1,2 +1,15 @@
 #coding:utf-8
+"""
+Opérateur de comparaison : == (égal a)
+						   != (différent de)
+						   < (Inferieur a)
+						   > (Superieur a)
+						   <=(Inferieur ou égal a)
+						   >=(Superieur ou égal )
 
+Mot clés des conditions : if / elif / else
+
+Conditions multiples	 : and (ET)
+						   or (OU)
+						   in / not in (DANS / PAS DANS)
+"""
